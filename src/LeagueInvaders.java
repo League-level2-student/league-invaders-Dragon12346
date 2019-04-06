a9e09e5db6439870b8d626719981547e9efa772c
ArielBaruch <league@WTS14-4.attlocal.net>
2019-04-05 18:26:21 -0700

    LeagueInvaders(WIP)

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,16 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	 JFrame frame = new JFrame();
+	 int WIDTH = 500;
+	 int HEIGHT = 800;
+public static void main(String[] args) {
+LeagueInvaders F = new LeagueInvaders();
+F.setup();
+}
+void setup(){
+	frame.setSize(WIDTH, HEIGHT);
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
 }
