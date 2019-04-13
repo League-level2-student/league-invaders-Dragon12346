3e3bee5953d179ecbf1ffbcd8abbdd5ea6474c29
Dragon12346 <league@WorthyMini-5.attlocal.net>
2019-04-12 18:27:13 -0700

    LeagueInvaders

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,45 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel{
-	
+	Font titleFont = new Font("Arial", Font.ITALIC, 48);
+	final int MENU = 0;
+    final int GAME = 1;
+    final int END = 2;
+    int currentState = MENU;
+	@Override
+	public void paintComponent(Graphics g){
+		if(currentState == MENU){
+		    drawMenuState(g);
+		}else if(currentState == GAME){
+		    drawGameState(g);
+		}else if(currentState == END){
+		    drawEndState(g);
+		}
+	}
+	 void updateMenuState() {  }
+	 void updateGameState() {  }
+	 void updateEndState()  {  }
+	 void drawMenuState(Graphics g) {
+		 g.setColor(Color.BLUE);
+		 g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);
+		 g.setFont(titleFont);
+		 g.setColor(Color.CYAN);
+		 g.drawString("text", LeagueInvaders.WIDTH/3, LeagueInvaders.HEIGHT/2);
+		 }
+	 
+	 
+	 void drawGameState(Graphics g) {
+		 g.setColor(Color.BLACK);
+		 g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);  }
+	 
+	 
+	 void drawEndState(Graphics g)  {
+		 g.setColor(Color.RED);
+		 g.fillRect(0, 0, LeagueInvaders.WIDTH, LeagueInvaders.HEIGHT);  }
+
 	
 }
--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -2,17 +2,19 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class LeagueInvaders {
+	GamePanel gp = new GamePanel();
 	 JFrame frame = new JFrame();
-	 int WIDTH = 500;
-	 int HEIGHT = 800;
+	 static int WIDTH = 500;
+	 static int HEIGHT = 800;
 public static void main(String[] args) {
 LeagueInvaders F = new LeagueInvaders();
 F.setup();
 }
 void setup(){
+	frame.add(gp);
 	frame.setSize(WIDTH, HEIGHT);
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.add(GamePanel);
+
 }
 }
