f62fd5cd44fb683eaf25f859667578165c4fd56d
League Student <league@iMac-87.attlocal.net>
2019-05-10 18:28:00 -0700

    LeagueInvaders(WIP)

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
 
 public class ObjectManager {
+Rocketship rocket;
+ArrayList<Projectile> projectiles;
+ArrayList<Alien> aliens;
+ObjectManager (Rocketship rocket){
+	this.rocket = rocket;
+}
+	void addProjectile() {
+		
+	}
 
 }
