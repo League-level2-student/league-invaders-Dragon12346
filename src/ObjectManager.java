7646deaa77129dd6f18053537e297f2440a246e0
League Student <league@iMac-87.attlocal.net>
2019-05-17 18:11:10 -0700

    LeagueInvaders(WIP)

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,14 +1,22 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ObjectManager {
 Rocketship rocket;
+Random random = new Random();
 ArrayList<Projectile> projectiles;
 ArrayList<Alien> aliens;
 ObjectManager (Rocketship rocket){
 	this.rocket = rocket;
 }
-	void addProjectile() {
+	void addProjectile(Projectile p) {
+		projectiles.add(p);
+	}
+	void addAlien() {
+		aliens.add(new Alien(random.nextInt(LeagueInvaders.WIDTH),0,50,50));
+	}
+	void update() {
 		
 	}
-
+	
 }
